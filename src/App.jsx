b615fa469035002a061febd3988e36ff1d3479fc
Franco rossi <rossifranco748@gmail.com>
2024-05-20 23:48:09 -0300

    Organizando archivos, creacion de Home y rutas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
-
+import { Link } from "react-router-dom";
+import "./app.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container_main">
+      <div className="container_card">
+        <img
+          className="logo"
+          src="/asets/logo_veterinaria-sinFondo.png"
+          alt="Logo Veterinaria"
+        />
+        <h2>Inicio de sesion</h2>
+
+        <div className="container_form">
+          <form action="#" method="POST">
+            <label htmlFor="email">Email</label>
+
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className=""
+            />
+
+            <label htmlFor="password" className="">
+              Password
+            </label>
+
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className=""
+            />
+
+            <a href="#" className="">
+              Forgot password?
+            </a>
+
+            <Link to={"/pagePrincipal"}>
+              <button type="submit" className="">
+                Enviar
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
