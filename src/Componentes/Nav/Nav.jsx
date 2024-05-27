1d8e36fb634cbef289dd6da96703ed849f0686ff
Franco rossi <rossifranco748@gmail.com>
2024-05-27 11:20:23 -0300

    Cambio de ruta de acceso para logo

--- src/Componentes/Nav/Nav.jsx
+++ src/Componentes/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import { FaDog, FaCalendarAlt, FaUsers } from "react-icons/fa";
 import { FaRegClipboard } from "react-icons/fa6";
 import "./nav.css";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
@@ -9,23 +10,33 @@ function Nav() {
         <div className="sidebar-content">
           <div className="sidebar-header">
             <a className="sidebar-link" href="#">
-              <img  className='sidebar-logo' src="asets/logo_veterinaria-sinFondo.png" alt="logo" />
+              <img
+                className="sidebar-logo"
+                src="/asets/logo_veterinaria-sinFondo.png"
+                alt="logo"
+              />
             </a>
           </div>
           <div className="sidebar-body">
             <nav className="nav">
-              <a className="nav-link" href="#">
-                <FaDog className="icon" />
-                Pacientes
-              </a>
-              <a className="nav-link" href="#">
-                <FaCalendarAlt className="icon" />
-                Citas
-              </a>
-              <a className="nav-link" href="#">
-                <FaRegClipboard className="icon" />
-                Fichas
-              </a>
+              <Link to={"/pagePrincipal"}>
+                <a className="nav-link" href="#">
+                  <FaDog className="icon" />
+                  Pacientes
+                </a>
+              </Link>
+              <Link>
+                <a className="nav-link" href="#">
+                  <FaCalendarAlt className="icon" />
+                  Citas
+                </a>
+              </Link>
+              <Link to={"/pageHistorial"}>
+                <a className="nav-link" href="#">
+                  <FaRegClipboard className="icon" />
+                  Historial Clínico
+                </a>
+              </Link>
               <a className="nav-link" href="#">
                 <FaUsers className="icon" />
                 Empleados
