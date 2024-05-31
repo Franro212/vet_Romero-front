eb7428a1bb5c88eaa9da36fd382b0b0271cb31dc
Franco rossi <rossifranco748@gmail.com>
2024-05-31 11:34:07 -0300

    Se agregan nuevas rutas para citas

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Principal from "./Pages/Principal/Principal.jsx";
+import Principal from "./Pages/Pacientes/Principal.jsx";
 import Historial from "./Pages/Historial/Historial.jsx";
 import HistorialClinico from "./Pages/Historial/HistorialClinico/HistorialClinico.jsx";
-
+import Citas from "./Pages/Citas/Citas.jsx";
 
 
 const router = createBrowserRouter([
@@ -14,18 +14,21 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-
   {
     path: "pagePrincipal",
     element: <Principal />,
   },
   {
     path: "pageHistorial",
-    element: <Historial/>,
+    element: <Historial />,
   },
   {
     path: "pageHistorial/historialClinico",
     element: <HistorialClinico />,
+  },
+  {
+    path: "pageCitas",
+    element: <Citas />,
   },
 ]);
 
